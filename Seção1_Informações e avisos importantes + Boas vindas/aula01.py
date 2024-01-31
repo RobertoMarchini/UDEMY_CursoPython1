1cd775495e50952d677c307bbf95e5b81b4975ee
RobertoMarchini <robertoasp2013@gmail.com>
2024-01-30 22:54:32 -0300

    mudando diretório

---
+++ Seção1_Informações e avisos importantes + Boas vindas/aula01.py
@@ -0,0 +1,12 @@
+# Permite escrever um comentário, comentário acima
+print(123) # comentário na frente
+# comentário abaixo
+print(123)
+
+""" 
+usar para escever suas notas - docstrings
+"""
+
+'''
+usar para escever suas notas - docstrings
+'''
